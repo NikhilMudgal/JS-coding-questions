225a41f40f62dee38a5c9158c12f5915db285ad7
Nikhil Mudgal <nikhilm2097@gmail.com>
2023-07-29 18:50:17 +0530

    Implemented combination sum -2 method 1

--- Recursion/combination-sum-2.js
+++ Recursion/combination-sum-2.js
@@ -1,26 +1,53 @@
-//Combination Sum 2 (Array must be sorted)
+// Given: Collection of number and a target number
+// O/P: all unique combinations in an array where sum is equal to target number
+// Condition: Array must be sorted. Combinations returned must be lexicographical sorted order.Each number can used at once. The solution set must not contain duplicate combinations
 
-var A = [1,1,1,2,3,4]
-var sum = 4;
+var A = [], target = 0, combinatioSumItems = new Set();
 
+// Method 1: 
+const combinationSum2 = (index, sum, arr) => {
 
-function findCombination(index, target, arr, ans) {
-    if (target === 0) {
-        console.log(arr);
+    if(index === A.length) {
+        if(sum === 0) {
+            combinatioSumItems.add(JSON.stringify(arr));
+        }
         return;
     }
 
-    for(let i = index; i< A.length; i++) {
-        if(i > index && A[i] === A[i-1]) {
-            continue;
-        }
-        if(A[i] > target) {
-            break;
-        };
-        arr.push(A[i])
-        findCombination(i+1, target - A[i], arr, ans);
-        arr.pop()
+    if(A[index] <= sum) {
+      arr.push(A[index]);
+      combinationSum2(index +1, sum- A[index], arr)
+      arr.pop()
     }
+    combinationSum2(index + 1, sum, arr)
+
 }
 
-findCombination(0, sum, [], [])
+export function combinationTwo(array, targetSum) {
+    A = array, target = targetSum;
+    const combinationArray = []
+    combinationSum2(0, target, [])
+    combinatioSumItems.forEach(e => console.log(e));
+    return combinatioSumItems;
+}
+
+// function findCombination(index, target, arr, ans) {
+//     if (target === 0) {
+//         console.log(arr);
+//         return;
+//     }
+
+//     for(let i = index; i< A.length; i++) {
+//         if(i > index && A[i] === A[i-1]) {
+//             continue;
+//         }
+//         if(A[i] > target) {
+//             break;
+//         };
+//         arr.push(A[i])
+//         findCombination(i+1, target - A[i], arr, ans);
+//         arr.pop()
+//     }
+// }
+
+// findCombination(0, sum, [], [])
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { combinationOne } from './Recursion/combination-sum-1';
-var items = combinationOne([2,3,6,7], 7);
+// var items = combinationOne([2,3,6,7], 7);
+
+import { combinationTwo } from './Recursion/combination-sum-2';
+var items = combinationTwo([1,1,1,2,2], 4);
+
 const opElement = document.getElementById("output");
 opElement.innerHTML  = items;
