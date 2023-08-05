70576c50238d835a0f026b6080c12c3c16e9b17a
Nikhil Mudgal <nikhilm2097@gmail.com>
2023-08-05 21:15:47 +0530

    Method 2 for Combination sum 2

--- Recursion/combination-sum-2.js
+++ Recursion/combination-sum-2.js
@@ -2,33 +2,56 @@
 // O/P: all unique combinations in an array where sum is equal to target number
 // Condition: Array must be sorted. Combinations returned must be lexicographical sorted order.Each number can used at once. The solution set must not contain duplicate combinations
 
-var A = [], target = 0, combinatioSumItems = new Set();
+var A = [], target = 0, combinationSumArrays = [];
+
+// combinatioSumItems = new Set(); // we are using set to avoid unique combinations to be used in method 1
 
 // Method 1: 
-const combinationSum2 = (index, sum, arr) => {
+// const combinationSum2 = (index, sum, arr) => {
 
-    if(index === A.length) {
-        if(sum === 0) {
-            combinatioSumItems.add(JSON.stringify(arr));
-        }
+//     if(index === A.length) {
+//         if(sum === 0) {
+//             combinatioSumItems.add(JSON.stringify(arr));
+//         }
+//         return;
+//     }
+
+//     if(A[index] <= sum) {
+//       arr.push(A[index]);
+//       combinationSum2(index +1, sum- A[index], arr)
+//       arr.pop()
+//     }
+//     combinationSum2(index + 1, sum, arr)
+
+// }
+
+// Method 2: we are using this method to reduce the time complexity of logn which was occuring due to insertion of data structure inside another data structure
+
+function combinationSum2(index, sum, arr) {
+    
+    if(sum === 0) {
+        combinationSumArrays.push([...arr]);
         return;
     }
-
-    if(A[index] <= sum) {
-      arr.push(A[index]);
-      combinationSum2(index +1, sum- A[index], arr)
-      arr.pop()
+    for(let i=index; i< A.length; i++) {
+        if(i> index && A[i] === A[i - 1]) {
+            continue;
+        }
+        if (A[i]> sum) {
+            break;
+        }
+        
+        arr.push(A[i])
+        combinationSum2(i + 1, sum - A[i], arr)
+        arr.pop()
     }
-    combinationSum2(index + 1, sum, arr)
-
 }
 
 export function combinationTwo(array, targetSum) {
     A = array, target = targetSum;
-    const combinationArray = []
     combinationSum2(0, target, [])
-    combinatioSumItems.forEach(e => console.log(e));
-    return combinatioSumItems;
+    combinationSumArrays.forEach(e => console.log(e));
+    return combinationSumArrays;
 }
 
 // function findCombination(index, target, arr, ans) {
